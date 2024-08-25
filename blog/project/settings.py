456b79e3e3df161a66df758ec3c626dba59a3bc5
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-25 11:15:50 +0300

    add docker-compose and Dockerfile for django services & gitignore

--- blog/project/settings.py
+++ blog/project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('BLOG_DB_NAME'),
+        'USER': os.environ.get('BLOG_DB_USER'),
+        'PASSWORD': os.environ.get('BLOG_DB_PASSWORD'),
+        'HOST': os.environ.get('BLOG_DB_HOST'),  # Use environment variable for host
+        'PORT': 5432,
     }
 }
 
