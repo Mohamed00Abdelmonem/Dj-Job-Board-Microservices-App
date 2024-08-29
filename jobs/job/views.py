0a6a5bad481990568ed4e36f5114ea60a05f3b61
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-29 20:09:15 +0300

    add custom job_paginations api

--- jobs/job/views.py
+++ jobs/job/views.py
@@ -4,13 +4,14 @@ from rest_framework import filters
 from .serializers import JobSerializer, JobApplySerializer
 from .models import Job, JobApply
 from .filters import JobFilter
-
+from .pagination import JobsPagination
 
 class JobList(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     filter_backends = [DjangoFilterBackend , filters.SearchFilter]
     filterset_class = JobFilter
+    pagination_class = JobsPagination
 
 
 
