de12ee4a9856623b49c84d19ebd31d8a847b8ad8
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-09-08 20:42:43 +0300

    add urls project for users-app

--- users/accounts/models.py
+++ users/accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser , BaseUserManager , PermissionsMixin
 from django.contrib.auth.hashers import make_password
-
 '''
     - user (fn,ln,email,password,username) + auth views + permissions + groups --> admin
     - add extra field : extend user:
@@ -10,45 +9,42 @@ from django.contrib.auth.hashers import make_password
         - abstractbaseuser --> low level --> hard code 
 '''
 
+
 class CustomUserManager(BaseUserManager):
-    ''' used to create user : password , create superuser'''
-    def create_user(self, email, password=None, **extra_fields):
+    ''' used to create user : password , create superuser '''
+    
+    def create_user(self,email,password=None,**extra_fields):
         if not email:
-            raise ValueError('The email must be set')
+            raise ValueError('the email field is required ...')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email,**extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-        return user
+        return user 
+        
     
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_active',True)
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        return self.create_user(email,password,**extra_fields)
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
-    is_active = models.BooleanField(default=False) # can login
-    is_staff = models.BooleanField(default=False) # can access admin
+    username = models.CharField(max_length=25)
+    is_active = models.BooleanField(default=False)   # can login 
+    is_staff = models.BooleanField(default=False)     # can login in admin
     date_joined = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
-
-
-    USERNAME_FIELD = 'email'
+    image = models.ImageField(upload_to='users/')
+    
+    USERNAME_FIELD = 'email'   # login with email 
     REQUIRED_FIELDS = []
-
+    
     objects = CustomUserManager()
-
-    def __str__(self) -> str:
+    
+    def __str__(self):
         return self.email
-
+    
--- users/project/urls.py
+++ users/project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -39,11 +39,14 @@ schema_view = get_schema_view(
 )
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/' , include('accounts.urls')),
+    
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
